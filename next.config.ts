2e4d843dc446fc6d88c678a0bb8cfd0b480c0aa7
Aman Verma <amanverma4816@gmail.com>
2026-01-14 22:29:59 +0530

    f

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ Allow warnings without breaking the build
-  },
   // Add other config options here as needed
 };
 
